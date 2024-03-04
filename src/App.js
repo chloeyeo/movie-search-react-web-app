43e92f68fe93d56777664588fd096d328d5b8d93
Chloe Yeo <yeochloe01@gmail.com>
2024-03-04 21:19:43 +0900

    react movie web app practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+import './App.css';
+import Movie from './Movie';
+
+// api key for omdb movies: 4626d5e6
+
+const API_URL = 'http://www.omdbapi.com?apikey=4626d5e6';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <h1>Search Omdb Movies</h1>
+      <div className="search">
+        <input
+        placeholder="Search your movie"
+        value={searchText}
+        onChange={(event)=> setSearchText(event.target.value)}
+        />
+        <img
+          src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+          alt="search icon"
+          onClick={()=>searchMovies(searchText)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie)=>(
+            <Movie movie={movie} />
+          ))}
+        </div>
+      ):(
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+
     </div>
   );
 }
